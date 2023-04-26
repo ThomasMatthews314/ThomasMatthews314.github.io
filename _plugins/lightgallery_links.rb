4ab5a50392a41ed12fce10f94303b157da7455ba
ThomasMatthews314 <108184300+ThomasMatthews314@users.noreply.github.com>
2023-04-26 09:48:34 -0400

    Update lightgallery_links.rb

--- _plugins/lightgallery_links.rb
+++ _plugins/lightgallery_links.rb
@@ -1,3 +1,3 @@
-Jekyll::Hooks.register :page, :pre_render do |page, payload|
+Jekyll::Hooks.register :pages, :pre_render do |page, payload|
   page.content.gsub!(/^!\[(.*)\]\(([^\)]+)\)((?:{:[^}]+})*)/, "<a href=\"{{ site.baseurl }}\\2\" class=\"lightgallery-link\" data-sub-html=\"\\1\">\n![\\1]({{ site.baseurl }}\\2){: height="" width=""}\\3{:data-src=\"{{ site.baseurl }}\\2\"}\n</a>")
 end
